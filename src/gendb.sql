0d7f3feaae5deeeb4dddc7ed450f95658623edbb
Piboonsak <Piboonsak@users.noreply.github.com>
2019-05-28 14:47:59 +0700

    add gendb.sql

---
+++ src/gendb.sql
@@ -0,0 +1,14 @@
+CREATE USER director IDENTIFIED BY P@ssw0rd;
+
+CREATE DATABASE directorexport CHARACTER SET utf8;
+GRANT ALL PRIVILEGES ON directorexport.* TO 'director'@'%';
+GRANT ALL PRIVILEGES ON directorexport.* TO 'director'@'localhost';
+GRANT ALL PRIVILEGES ON directorexport.* TO 'root'@'localhost';
+
+CREATE DATABASE director CHARACTER SET utf8;
+GRANT ALL PRIVILEGES ON director.* TO 'director'@'%';
+GRANT ALL PRIVILEGES ON director.* TO 'director'@'localhost';
+GRANT ALL PRIVILEGES ON director.* TO 'root'@'localhost' ;
+
+grant all on directorexport.* TO 'root'@'%' IDENTIFIED BY 'P@ssw0rd' with grant option;
+grant all on director.* TO 'root'@'%' IDENTIFIED BY 'P@ssw0rd' with grant option;
