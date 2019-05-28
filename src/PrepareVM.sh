e3c39939b21eb088628f733fd58ab3b4e670fc1d
Piboonsak <Piboonsak@users.noreply.github.com>
2019-05-28 11:21:27 +0700

    Update PrepareVM.sh

--- src/PrepareVM.sh
+++ src/PrepareVM.sh
@@ -10,3 +10,16 @@ yum install -y git
 
 ### [Install mysql Follow this Link](https://www.digitalocean.com/community/tutorials/how-to-install-mysql-on-centos-7)
 
+wget https://dev.mysql.com/get/mysql57-community-release-el7-9.noarch.rpm
+
+md5sum mysql57-community-release-el7-9.noarch.rpm
+
+sudo rpm -ivh mysql57-community-release-el7-9.noarch.rpm
+
+sudo yum install mysql-server
+
+sudo systemctl status mysqld
+
+[root@altus ~]# sudo grep 'temporary password' /var/log/mysqld.log
+2019-05-28T04:18:12.811710Z 1 [Note] A temporary password is generated for root@localhost: JFqL6TWaxr#n
+
