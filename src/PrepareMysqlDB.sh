7f016a2a68ed6f892d520cff9e900b3994962f2f
Piboonsak <Piboonsak@users.noreply.github.com>
2019-05-28 14:09:14 +0700

    enable mysql in service

--- src/PrepareMysqlDB.sh
+++ src/PrepareMysqlDB.sh
@@ -13,4 +13,8 @@ chown mysql:mysql /var/log/mysql/bk
 mv /var/lib/mysql/ib_logfile0 /var/lib/mysql/bk/ib_logfile0
 mv /var/lib/mysql/ib_logfile1 /var/lib/mysql/bk/ib_logfile1
 
+# enable mysql in service
+sudo /sbin/chkconfig --list mysqld mysqld 0:off 1:off 2:on 3:on 4:on 5:on 6:off
 
+#Start mysql
+sudo service mysqld start
