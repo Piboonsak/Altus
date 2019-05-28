cff4fcded993e6549a7f76f839c80c9190c9b9a5
Piboonsak <Piboonsak@users.noreply.github.com>
2019-05-28 12:44:51 +0700

    Update PrepareVM.sh

--- src/PrepareVM.sh
+++ src/PrepareVM.sh
@@ -8,7 +8,7 @@ yum install -y nano
 
 yum install -y git
 
-### [Install mysql Follow this Link](https://www.digitalocean.com/community/tutorials/how-to-install-mysql-on-centos-7)
+## [Install mysql Follow this Link](https://www.digitalocean.com/community/tutorials/how-to-install-mysql-on-centos-7)
 
 wget https://dev.mysql.com/get/mysql57-community-release-el7-9.noarch.rpm
 
@@ -22,5 +22,6 @@ sudo systemctl status mysqld
 
 sudo grep 'temporary password' /var/log/mysqld.log
 
-##output should be 2019-05-28T04:18:12.811710Z 1 [Note] A temporary password is generated for root@localhost: JFqL6TWaxr#n
+## output should be 2019-05-28T04:18:12.811710Z 1 [Note] A temporary password is generated for root@localhost: JFqL6TWaxr#n
 
+sudo mysql_secure_installation
