676e5a560b3736f4599edbb5e7e1d92af6ed3412
Piboonsak <Piboonsak@users.noreply.github.com>
2019-05-28 13:00:13 +0700

    Update PrepareMysqlDB.sh

--- src/PrepareMysqlDB.sh
+++ src/PrepareMysqlDB.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 ## For Prepare mysql
-
 sudo service mysqld stop
 
+### Create slow log for mysql
+mkdir -p /var/log/mysql-slow.log
+
 ### After Stop Mysql server
-
 mkdir -p /var/lib/mysql/bk
-
 mv /var/lib/mysql/ib_logfile0 /var/lib/mysql/bk/ib_logfile0
-
 mv /var/lib/mysql/ib_logfile1 /var/lib/mysql/bk/ib_logfile1
 
 
